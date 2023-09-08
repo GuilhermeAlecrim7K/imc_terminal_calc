1e95e802a4828c0efcc5c08ad866aade1dc86572
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2023-09-07 22:32:45 -0300

    create template package

---
+++ bin/imc_terminal_calc.dart
@@ -0,0 +1,5 @@
+import 'package:imc_terminal_calc/imc_terminal_calc.dart' as imc_terminal_calc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imc_terminal_calc.calculate()}!');
+}
---
+++ test/imc_terminal_calc_test.dart
@@ -0,0 +1,8 @@
+import 'package:imc_terminal_calc/imc_terminal_calc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

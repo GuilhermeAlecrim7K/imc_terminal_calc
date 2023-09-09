169b0097efb933b8959e428eb1d0851c0c3d29c1
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2023-09-09 17:48:10 -0300

    setup user input

--- bin/imc_terminal_calc.dart
+++ bin/imc_terminal_calc.dart
@@ -1,5 +1,5 @@
 import 'package:imc_terminal_calc/imc_terminal_calc.dart' as imc_terminal_calc;
 
-void main(List<String> arguments) {
-  print('Hello world: ${imc_terminal_calc.calculate()}!');
+void main() {
+  imc_terminal_calc.run();
 }
--- lib/imc_terminal_calc.dart
+++ lib/imc_terminal_calc.dart
@@ -1,3 +1,35 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+void run() {
+  while (true) {
+    stdout.writeln('=======================================');
+    stdout.writeln('        IMC Terminal Calculator        ');
+    stdout.writeln('=======================================');
+    stdout.write('Type in your name: ');
+    String? name = stdin.readLineSync();
+    while (name == null || name.isEmpty) {
+      stdout.write("Invalid. Type in your name: ");
+      name = stdin.readLineSync();
+    }
+
+    stdout.write('Type in your height(in meters): ');
+    double? heightInMeters = double.tryParse(stdin.readLineSync() ?? '');
+    while (heightInMeters == null) {
+      stdout.write("Invalid. Type in your height: ");
+      heightInMeters = double.tryParse(stdin.readLineSync() ?? '');
+    }
+
+    stdout.write('Type in your weight(in kilograms): ');
+    double? weightInKilograms = double.tryParse(stdin.readLineSync() ?? '');
+    while (weightInKilograms == null) {
+      stdout.write("Invalid. Type in your weight: ");
+      weightInKilograms = double.tryParse(stdin.readLineSync() ?? '');
+    }
+
+    //TODO: calculate IMC
+
+    stdout.write('Would you like to try again? (Y/N):');
+    final tryAgainResponse = stdin.readLineSync();
+    if (!['Y', 'YES'].contains(tryAgainResponse?.toUpperCase())) break;
+  }
 }
